ea478429fc30ec010ca34386a0a4da3c4cafc4b4
aymericf <aymeric.flegeo@blablacar.com>
2023-06-18 08:50:23 +0200

    Schema Registry setup

--- README.md
+++ README.md
@@ -2,11 +2,24 @@
 
 ## Installation
 
-Create topic:
+### Start Kafka cluster:
+```bash
+docker-compose up -d --remove-orphans
+```
 
+
+### Create topic:
 ```bash
 docker exec broker \
 kafka-topics --bootstrap-server broker:9092 \
              --create \
              --topic transaction
+```
+
+### Create a new Avro schema:
+Create a new Avro schema file in `event/`
+
+Then, to send schema to Schema Registry:
+```bash
+sh utils/send_schema_to_schema_registry.sh schema-name
 ```
--- utils/send_schema_to_schema_registry.sh
+++ utils/send_schema_to_schema_registry.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-export SCHEMA=$(cat event/$1.avsc | tr -d '\n\t\r ')
-export SCHEMA_JSON=$(echo '{"schema":""}' | jq --arg schema "$SCHEMA" '.schema = $schema')
-echo $SCHEMA_JSON
+export SCHEMA=$(cat event/$1.avsc | tr '\n' ' ' | sed 's/"/\\"/g')
 curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" \
-    --data $(echo $SCHEMA_JSON) localhost:8081//subjects/$1/versions
+    --data '{"schema": "'"$SCHEMA"'"}' http://localhost:8081/subjects/$1/versions
