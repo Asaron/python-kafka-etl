71f27f8b90b354f0185ec9fb7de3dbb6589312e8
aymericf <aymeric.flegeo@blablacar.com>
2023-06-20 08:39:22 +0200

    Init clickhouse client

---
+++ src/clikhouse_sink.py
@@ -0,0 +1,21 @@
+import clickhouse_connect
+
+CLIENT = clickhouse_connect.get_client(host='localhost', username='etl', password='password')
+
+class ClickhouseSink:
+
+    @staticmethod
+    def insert_rows(table: str, data: object):
+        print(f"data: {data}")
+        CLIENT.insert(table, [data], column_names=[
+            "event_id",
+            "timestamp",
+            "consumer_id",
+            "bank_id",
+            "amount",
+            "country_code",
+            "execution_date",
+            "merchant_id"
+            ])
+
+    
